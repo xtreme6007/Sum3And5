92da4728308806fb5fc7a718e480154ecdce2d0b
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 11:23:24 -0600

    added system print

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         for(int i = 1; i < 1000; i++){
                 if(((i % 5)== 0) && ((i % 3) == 0)){
                     sum += i;
-
+                    System.out.println("Sum:" + sum);
                 }
         }
     }
