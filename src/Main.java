2d6f79c72519533cb1c3339ca4ff6a779971085e
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 11:27:33 -0600

    added conditonal to break when count equals 5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args){
+            int count = 0;
             int sum = 0;
         for(int i = 1; i < 1000; i++){
                 if(((i % 5)== 0) && ((i % 3) == 0)){
                     sum += i;
-                    System.out.println("Sum:" + sum);
+                    count++;
+                    System.out.println("Sum:" + sum + " at count " + count);
+                }
+                if(count == 5){
+                    System.out.println("Exiting The Looptrix");
+                    break;
                 }
         }
     }
